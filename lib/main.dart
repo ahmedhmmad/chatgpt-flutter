c40714cdefd78acb1ae6800523e925b2a0efc2ee
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2023-03-14 07:42:53 +0200

    done input api key from user

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
-import 'package:chatgptapp/providers/apikey_provider.dart';
-import 'package:chatgptapp/providers/models_provider.dart';
-import 'package:chatgptapp/providers/theme_provider.dart';
-import 'package:chatgptapp/screens/chat_screen.dart';
-import 'package:chatgptapp/screens/enter_api_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-// import 'screens/home_screen.dart';
+import 'providers/apikey_provider.dart';
+import 'providers/models_provider.dart';
+import 'providers/theme_provider.dart';
+import 'screens/chat_screen.dart';
+import 'screens/enter_api_screen.dart';
 
 void main() {
   runApp(ChangeNotifierProvider<ThemeProvider>(
@@ -17,46 +16,57 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  bool isApiSet() {
-    SharedPreferences.getInstance().then((prefs) {
-      String apiKey = prefs.getString('apiKey') ?? '';
-      if (apiKey == '') {
-        print('API Key not set');
-        return false;
-      } else {
-        print('API Key set');
-        return true;
-      }
-    });
-    print('exit');
-    return true;
+  const MyApp({Key? key}) : super(key: key);
+
+  Future<bool> isApiSet() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String apiKey = prefs.getString('apiKey') ?? '';
+    if (apiKey == '') {
+      print('API Key not set');
+      return false;
+    } else {
+      print('API Key set');
+      return true;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          Provider<ThemeProvider>(create: (_) => ThemeProvider()..getTheme()),
-          Provider<ModelsProvider>(create: (_) => ModelsProvider()),
-          Provider<ApiProvider>(create: (_) => ApiProvider()..getApiKey()),
-          // Provider<AnotherThing>(create: (_) => AnotherThing()),
-        ],
-
-        //   MultiProvider(
-        //     providers:[
-        //     ChangeNotifierProvider(
-        //       Provider<ThemeProvider>(create: (_)=>ThemeProvider()..getTheme(),),
-        //       Provider<ModelsProvider>(create: (_)=>ModelsProvider()..getModels()),
-        //     ),
-        //     ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'ChatGPT',
-          theme: ThemeData.light(),
-          darkTheme: ThemeData.dark(),
-          themeMode: Provider.of<ThemeProvider>(context).themeMode,
-          home: isApiSet() ? ChatScreen() : const EnterApiScreen(),
-        ));
+      providers: [
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (_) => ThemeProvider()..getTheme(),
+        ),
+        Provider<ModelsProvider>(
+          create: (_) => ModelsProvider(),
+        ),
+        Provider<ApiProvider>(
+          create: (_) => ApiProvider()..getApiKey(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'ChatGPT',
+        theme: ThemeData.light(),
+        darkTheme: ThemeData.dark(),
+        themeMode: Provider.of<ThemeProvider>(context).themeMode,
+        home: FutureBuilder<bool>(
+          future: isApiSet(),
+          builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Container(
+                color: Colors.white,
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            } else {
+              bool apiSet = snapshot.data ?? false;
+              return apiSet ? ChatScreen() : EnterApiScreen();
+            }
+          },
+        ),
+      ),
+    );
   }
 }
